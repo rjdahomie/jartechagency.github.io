ac82c43c008e6090a81bc8b4fe860f7b97de7dda
adhith <24882134+kkadhith@users.noreply.github.com>
2023-12-25 16:58:48 -0800

    Added logo, made text bigger

--- Fanatic-React-Template/src/components/Nav/Sidebar.jsx
+++ Fanatic-React-Template/src/components/Nav/Sidebar.jsx
@@ -4,24 +4,25 @@ import { Link } from "react-scroll";
 // Assets
 import CloseIcon from "../../assets/svg/CloseIcon";
 import LogoIcon from "../../assets/svg/Logo";
+import { ReactComponent as SidebarLogo } from '../../assets/svg/Logo.svg';
 
 export default function Sidebar({ sidebarOpen, toggleSidebar }) {
   return (
     <Wrapper className="animate darkBg" sidebarOpen={sidebarOpen}>
       <SidebarHeader className="flexSpaceCenter">
-        <div className="flexNullCenter">
-          <LogoIcon />
-          <h1 className="whiteColor font20" style={{ marginLeft: "15px" }}>
-            fanatic
-          </h1>
-        </div>
-        <CloseBtn onClick={() => toggleSidebar(!sidebarOpen)} className="animate pointer">
-          <CloseIcon />
-        </CloseBtn>
-      </SidebarHeader>
+      <div className="flexNullCenter">
+    <SidebarLogo style={{ width: '150px', height: '150px',  filter: 'invert(100%)'}} />
+    {/* <h1 className="whiteColor font20" style={{ marginLeft: "1px" }}>
+      fanatic
+    </h1> */}
+  </div>
+  <CloseBtn onClick={() => toggleSidebar(!sidebarOpen)} className="animate pointer">
+    <CloseIcon />
+  </CloseBtn>
+</SidebarHeader>
 
       <UlStyle className="flexNullCenter flexColumn">
-        <li className="semiBold font15 pointer">
+        <li className="semiBold font20 pointer">
           <Link
             onClick={() => toggleSidebar(!sidebarOpen)}
             activeClass="active"
@@ -35,7 +36,7 @@ export default function Sidebar({ sidebarOpen, toggleSidebar }) {
             Home
           </Link>
         </li>
-        <li className="semiBold font15 pointer">
+        <li className="semiBold font20 pointer">
           <Link
             onClick={() => toggleSidebar(!sidebarOpen)}
             activeClass="active"
@@ -49,35 +50,7 @@ export default function Sidebar({ sidebarOpen, toggleSidebar }) {
             Services
           </Link>
         </li>
-        <li className="semiBold font15 pointer">
-          <Link
-            onClick={() => toggleSidebar(!sidebarOpen)}
-            activeClass="active"
-            className="whiteColor"
-            style={{ padding: "10px 15px" }}
-            to="projects"
-            spy={true}
-            smooth={true}
-            offset={-60}
-          >
-            Projects
-          </Link>
-        </li>
-        <li className="semiBold font15 pointer">
-          <Link
-            onClick={() => toggleSidebar(!sidebarOpen)}
-            activeClass="active"
-            className="whiteColor"
-            style={{ padding: "10px 15px" }}
-            to="blog"
-            spy={true}
-            smooth={true}
-            offset={-60}
-          >
-            Blog
-          </Link>
-        </li>
-        <li className="semiBold font15 pointer">
+        <li className="semiBold font20 pointer">
           <Link
             onClick={() => toggleSidebar(!sidebarOpen)}
             activeClass="active"
@@ -91,7 +64,7 @@ export default function Sidebar({ sidebarOpen, toggleSidebar }) {
             Pricing
           </Link>
         </li>
-        <li className="semiBold font15 pointer">
+        <li className="semiBold font20 pointer">
           <Link
             onClick={() => toggleSidebar(!sidebarOpen)}
             activeClass="active"
@@ -104,18 +77,6 @@ export default function Sidebar({ sidebarOpen, toggleSidebar }) {
           >
             Contact
           </Link>
-        </li>
-      </UlStyle>
-      <UlStyle className="flexSpaceCenter">
-        <li className="semiBold font15 pointer">
-          <a href="/" style={{ padding: "10px 30px 10px 0" }} className="whiteColor">
-            Log in
-          </a>
-        </li>
-        <li className="semiBold font15 pointer flexCenter">
-          <a href="/" className="radius8 lightBg" style={{ padding: "10px 15px" }}>
-            Get Started
-          </a>
         </li>
       </UlStyle>
     </Wrapper>
